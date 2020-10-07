3803ed141208e44951110b4298aa4fe2d54dd5d7
Aaron Marino <aaron_marino@yahoo.com>
2020-10-07 11:18:15 -0400

    creation of mocks testing with get and create, libs directory for dynamodb and handler

---
+++ notes-app-api/create.js
@@ -0,0 +1,31 @@
+import * as uuid from "uuid";
+import handler from "./libs/handler-lib";
+import dynamoDb from "./libs/dynamodb-lib";
+
+export const main = handler(async (event, context) => {
+    //Request body is passed in as a JSON encoded string in 'event.body'
+    const data = JSON.parse(event.body);
+    const params = {
+        TableName: process.env.tableName,
+        // 'Item' contains the attributes of the item to becreated
+        // - 'userId': user identities are federated through the
+        // Cognito Identity Pool, we will usethe identity id
+        // as the user id of the authenticateduser
+        // - 'noteId': a unique uuid
+        // - 'content': parsed from request body
+        // - 'attachment': parsed from request body
+        // - 'createdAt': current Unix timestampItem:{userId:event.requestContext.identity.cognitoIdentityId,noteId:uuid.v1(),content:data.content,attachment:data.attachment,createdAt:Date.now()}};awaitdynamoDb.put(params);returnparams.Item;});async
+
+        Item: {
+            userId: event.requestContext.identity.cognitoIdentityId,
+            noteId: uuid.v1(),
+            content: data.content,
+            attachment: data.attachment,
+            createdAt: Date.now()
+        }
+    };
+
+    await dynamoDb.put(params);
+
+    return params.Item;
+});
